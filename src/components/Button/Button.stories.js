841a150ff564a199cd13b48b0df66de4735a9bb2
lucas.j <lucas.j@hpcnt.com>
2021-06-01 21:47:50 +0900

    add: subscription example

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-  title: 'Button', //left side panel
+  title: 'Form/Button', //left side panel
   component:Button
 }
 
--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -2,10 +2,12 @@ import React from "react"
 import Input from "./Input"
 
 export default {
-  title: 'Input',
+  title: 'Form/Input',
   component:Input
 }
 
 export const Small = () => <Input placeholder={`Small size`} size={'small'}/>
 export const Medium = () => <Input placeholder={`Medium size`} size={'medium'}/>
 export const Large = () => <Input placeholder={`Large size`} size={'large'}/>
+
+Small.storyName = 'Small Input'
