e53dc98d8ce1c37c76c54188c1a84449ae209d3a
lucas.j <lucas.j@hpcnt.com>
2021-06-01 21:30:36 +0900

    add: Input storybook example

---
+++ src/components/Input/Input.stories.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Input from "./Input"
+
+export default {
+  title: 'Input',
+  component:Input
+}
+
+export const Small = () => <Input placeholder={`Small size`} size={'small'}/>
+export const Medium = () => <Input placeholder={`Medium size`} size={'medium'}/>
+export const Large = () => <Input placeholder={`Large size`} size={'large'}/>
