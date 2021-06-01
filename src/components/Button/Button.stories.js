bf666942e58993779f5ea5576b01a5e247b1e834
lucas.j <lucas.j@hpcnt.com>
2021-06-01 21:22:11 +0900

    add: Button storybook example code

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./Button";
+
+export default {
+  title: 'Button', //left side panel
+  component:Button
+}
+
+// export each button 
+export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>
+export const Success = () => <Button variant='success'>Success</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
