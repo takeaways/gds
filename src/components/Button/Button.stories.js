7d94679e01cc4bbd33697f06ffeedbbe9bbe61e1
lucas.j <lucas.j@hpcnt.com>
2021-06-01 21:59:43 +0900

    add: Center decorator

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Center from "../Center/Center"
 
 export default {
   title: 'Form/Button', //left side panel
@@ -10,10 +11,10 @@ export default {
 }
 
 // export each button 
-export const Primary = () => <Button variant='primary'>Primary</Button>
-export const Secondary = () => <Button variant='secondary'>Secondary</Button>
-export const Success = () => <Button variant='success'>Success</Button>
-export const Danger = () => <Button variant='danger'>Danger</Button>
+export const Primary = () => <Center><Button variant='primary'>Primary</Button></Center>
+export const Secondary = () => <Center><Button variant='secondary'>Secondary</Button></Center>
+export const Success = () => <Center><Button variant='success'>Success</Button></Center>
+export const Danger = () => <Center><Button variant='danger'>Danger</Button></Center>
 
 const Template = args => <Button {...args}/>
 export const PrimaryA = Template.bind({})
